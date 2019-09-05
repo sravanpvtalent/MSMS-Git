9c76ab72d834d619e9283709d92add3b1e8232c8
sravanpvtalent <sravanp.vtalent@gmail.com>
2019-09-05 15:07:01 +0530

    MSMS Project with Areas And Repositories Added

---
+++ MSMS/Areas/Admin/BussinessAccessLayer/bal.cs
@@ -0,0 +1,18 @@
+﻿using MSMS.Areas.Admin.IRepository;
+using MSMS.Areas.Admin.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSMS.Areas.Admin.BussinessAccessLayer
+{
+    public class bal
+    {
+        IRepos Iobj;
+        public bal(Repos Robj)
+        {
+            Iobj = Robj;
+        }
+    }
+}
---
+++ MSMS/Areas/Admin/Repository/Repos.cs
@@ -0,0 +1,12 @@
+﻿using MSMS.Areas.Admin.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSMS.Areas.Admin.Repository
+{
+    public class Repos:IRepos
+    {
+    }
+}
